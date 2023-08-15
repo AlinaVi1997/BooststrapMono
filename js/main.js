ea695de173b78f692b29399f0c278daac9a602e5
Alina <alinavyazovtseva@gmail.com>
2023-08-15 17:04:34 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// section comments slider//
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    speed: 800,
+    freeMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-arrow-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+        breakpoints: {
+            320: {
+            slidesPerView: 1,
+        },
+            600: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+// header-hamburger-menu
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+});
+
+
